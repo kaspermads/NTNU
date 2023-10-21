8b1540db44690b844a89e14c242bc86b6f8dbfa0
kaspermads <kaspergm@hotmail.no>
2023-10-21 21:06:05 +0200

    update build.hs

--- IELET2001_Prosjekt/datakom_prosjekt/datakom_prosjekt/settings.py
+++ IELET2001_Prosjekt/datakom_prosjekt/datakom_prosjekt/settings.py
@@ -72,8 +72,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'datakom_prosjekt.wsgi.application'
-STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'static'
+STATICFILES_DIRS = [BASE_DIR/'staticfiles',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 
 # Database
@@ -135,8 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
