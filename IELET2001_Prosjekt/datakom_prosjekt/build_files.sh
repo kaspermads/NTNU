78acab21f1432ad537c6c42d0a341ab2cbf5bc74
kaspermads <kaspergm@hotmail.no>
2023-10-21 19:34:45 +0200

    Deployment test

--- IELET2001_Prosjekt/datakom_prosjekt/build_files.sh
+++ IELET2001_Prosjekt/datakom_prosjekt/build_files.sh
@@ -1,2 +1,2 @@
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python3.11.6 manage.py collectstatic --noinput
--- IELET2001_Prosjekt/datakom_prosjekt/datakom_prosjekt/settings.py
+++ IELET2001_Prosjekt/datakom_prosjekt/datakom_prosjekt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d0)q=!tzda%&6*@d4$7sfmi&0o6r58p2q+=lwxixqjow17uz2e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['vercel.app', "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['.vercel.app', "localhost", "127.0.0.1"]
 
 
 # Application definition
