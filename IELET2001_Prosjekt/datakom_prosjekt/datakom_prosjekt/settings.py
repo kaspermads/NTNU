d01cde11771d49681fe97e1af88e8fdca3a52199
kaspermads <kaspergm@hotmail.no>
2023-10-21 18:49:27 +0200

    Test

--- IELET2001_Prosjekt/datakom_prosjekt/datakom_prosjekt/settings.py
+++ IELET2001_Prosjekt/datakom_prosjekt/datakom_prosjekt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d0)q=!tzda%&6*@d4$7sfmi&0o6r58p2q+=lwxixqjow17uz2e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['vercel.app', "localhost", "127.0.0.1"]
 
