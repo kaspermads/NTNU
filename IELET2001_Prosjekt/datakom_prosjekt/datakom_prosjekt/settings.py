035b83fbbc5364d0a105540576a5b313a0b71b82
kaspermads <kaspergm@hotmail.no>
2023-10-21 23:12:46 +0200

    update python

--- IELET2001_Prosjekt/datakom_prosjekt/datakom_prosjekt/settings.py
+++ IELET2001_Prosjekt/datakom_prosjekt/datakom_prosjekt/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+# STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 
 
 # Default primary key field type
