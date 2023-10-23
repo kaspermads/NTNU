bbff88efc4bedd942000fb2e746767e2baf4166e
kaspermads <kaspergm@hotmail.no>
2023-10-23 21:55:19 +0200

    Add ability to upload photo to pasient instance

--- IELET2001_Prosjekt/datakom_prosjekt/datakom_prosjekt/settings.py
+++ IELET2001_Prosjekt/datakom_prosjekt/datakom_prosjekt/settings.py
@@ -182,10 +182,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+
 # Use 'staticfiles' as STATIC_ROOT
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Default primary key field type
