02f4b81dfb1bd2e816b05acd69ccfd83dc20a5f4
kaspermads <kaspergm@hotmail.no>
2023-11-01 15:50:08 +0100

    update requirements.txt

--- IELET2001_Prosjekt/datakom_prosjekt/datakom_prosjekt/settings.py
+++ IELET2001_Prosjekt/datakom_prosjekt/datakom_prosjekt/settings.py
@@ -194,8 +194,8 @@ USE_TZ = True
 AZURE_ACCOUNT_NAME = 'kaspergmstorage'
 
 # Tell Django to use Azure storage backend:
-DEFAULT_FILE_STORAGE = 'datakom_prosjekt.custom_azure.AzureMediaStorage'
-STATICFILES_STORAGE = 'datakom_prosjekt.custom_azure.AzureStaticStorage'
+DEFAULT_FILE_STORAGE = 'custom_azure.AzureMediaStorage'
+STATICFILES_STORAGE = 'custom_azure.AzureStaticStorage'
 
 AZURE_STATIC_LOCATION = 'static'
 AZURE_MEDIA_LOCATION = 'media'
