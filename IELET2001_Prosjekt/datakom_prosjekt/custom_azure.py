7ec21714bb86c565753080318449e8ddb1e6b8ad
kaspermads <kaspergm@hotmail.no>
2023-11-08 13:01:25 +0100

    Remove environment variables

--- IELET2001_Prosjekt/datakom_prosjekt/custom_azure.py
+++ IELET2001_Prosjekt/datakom_prosjekt/custom_azure.py
@@ -1,17 +1,22 @@
 from storages.backends.azure_storage import AzureStorage
 import os
-AZURE_ACCOUNT_KEY = os.environ.get("AZURE_ACCOUNT_KEY")
+AZURE_ACCOUNT_KEY = 'mC5obmcDPKhswBnqwN5bc6D33CXQTDiTgHpYNOmeqTewa7aWTn78D+N1TmJ6pgV1069iWLWzrh4u+AStEwA7yQ=='
+# os.environ.get("AZURE_ACCOUNT_KEY")
 
 
 class AzureMediaStorage(AzureStorage):
-    account_name = os.environ.get("AZURE_ACCOUNT_NAME")  # Must be replaced by your <storage_account_name>
+    # Must be replaced by your <storage_account_name>
+    account_name = 'kaspergmstorage'
+    # os.environ.get("AZURE_ACCOUNT_NAME")
     account_key = AZURE_ACCOUNT_KEY  # Must be replaced by your <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
 
 
 class AzureStaticStorage(AzureStorage):
-    account_name = os.environ.get("AZURE_ACCOUNT_NAME")  # Must be replaced by your storage_account_name
+    # Must be replaced by your storage_account_name
+    account_name = 'kaspergmstorage'
+    # os.environ.get("AZURE_ACCOUNT_NAME")
     account_key = AZURE_ACCOUNT_KEY  # Must be replaced by your <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
